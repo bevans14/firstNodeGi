bdebd14882ce2cd79c46fee0f4356266f001f4ac
bevans14 <146108421+bevans14@users.noreply.github.com>
2023-12-10 14:17:03 -0500

    commit

--- baby-steps.js
+++ baby-steps.js
@@ -2,6 +2,7 @@
 let arguments = process.argv.length; 
 
 let sum = 0;
+
 for (let i=2; i < arguments; i++){  
 sum += Number(process.argv[i]);
         
--- hard-challenge/app.js
+++ hard-challenge/app.js
@@ -1,19 +1,50 @@
-const fs = require('fs')
-const express = require('express')
-const app = express();
-const port = 3000;
-
-// exporting
-const employeeData = require('./employees.json')
-const data = JSON.stringify(employeeData)
-fs.writeFileSync('employees.json', data, 'utf8' )
+const express = require('express');
+const fs = require('fs');
+const employeeData= require('./employees.json')
 
 
-app.get('/', (req, res) => {
-res.json(`${employeeData}`)
+  
+// const employeeDataString = JSON.stringify(employeeDataBuffer)
+// const employeeData = JSON.parse(employeeDataBuffer);
+
+const app = express()
+
+// send
+
+app.get('', (req, res) => {
+    res.send('just a regular page with no emplyee data at all :)')
+})
+
+app.get('/employees', (req, res) => {
+    res.send(employeeData)
 })
 
 
-app.listen(port, () => {
-    console.log(`${employeeData.name}`)
+app.get('/employees/:employeeID', (req, res) => {
+    const employeeID = parseInt(req.params.employeeID);
+    
+    const employee = employeeData.find(emp => emp.employeeID === employeeID);
+
+    if (employee) {
+        res.json(employee); 
+    } else {
+        res.status(404).json('404 error');
+    }
+});
+
+
+// now need to start server
+
+app.listen(3000, () => {
+    console.log('server is up port 3000')
 })
+
+// port is still open until we close it
+
+
+
+
+
+
+
+
